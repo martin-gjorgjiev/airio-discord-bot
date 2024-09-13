62d84f7f553137baf2c8558896d4619955fe28ca
martin-gjorgjiev <martin.gorgiev@rocketmail.com>
2024-09-13 16:52:00 +0200

    logging and fix for zero days string

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from discord.ext import tasks
 import asyncio
 import csv
 from datetime import datetime,timedelta
+import logging
 
 # files from env
 load_dotenv()
@@ -17,23 +18,28 @@ CHANNEL: Final[str] = os.getenv('CHANNEL_ID')
 intents: Intents = Intents.default()
 intents.message_content = True
 client: Client = Client(intents=intents)
+logger=logging.getLogger(__name__)
+logging.basicConfig(filename='log.txt', level=logging.NOTSET,format='%(asctime)s - %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
 
 # on start
 @client.event
 async def on_ready() -> None:
     print(f'{client.user} is now running!')
-    print(str(AIRIO))
+    logger.info(f'{client.user} is now running!')
     bans_hourly.start()
 
 # ban list check task
 @tasks.loop(hours = 1)
 async def bans_hourly() -> None:
+    logger.info('Hourly called')
     channel = client.get_channel(int(CHANNEL))
     last_hour_date_time = datetime.now() - timedelta(hours = 1)
     with open(str(AIRIO)+'/Airio.ban.txt',newline='') as csvfile:
         reader = csv.reader(csvfile,delimiter='	')
         for row in reader:
             if datetime.strptime(row[3],'%Y-%m-%d %H:%M:%S')>last_hour_date_time:
+                if row[4]==0:
+                    row[4]='half'
                 msgStr = f"{row[3]}: User `{row[0]}` recieved a {row[4]} day ban for reason '{row[5]}'. Responsible limad `{row[2]}`."
                 await channel.send(msgStr)
 

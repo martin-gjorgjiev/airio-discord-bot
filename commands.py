481d30910eb82401aa769ad551c975cd83306494
martin-gjorgjiev <martin.gorgiev@rocketmail.com>
2024-12-05 16:51:39 +0100

    command bug fix

--- commands.py
+++ commands.py
@@ -1,4 +1,5 @@
 def command_resolve(user_input: str)->str:
-    if("!ping"):
-        return "Pong"
-    return "Invalid command"
+    if(user_input[0]=='!'):
+        if(user_input=="!ping"):
+            return "Pong"
+        return "Invalid command"
